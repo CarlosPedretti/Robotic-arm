ed0c9c4379e366458b10555f570e3c056ee94144
MarioLDD <mariodedonatis@hotmail.com>
2023-11-22 23:38:09 -0300

    First commit

---
+++ Assets/Scripts/ArmController.cs
@@ -0,0 +1,83 @@
+using Mono.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using DG.Tweening;
+public class ArmController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody baseRotorRb;
+    [SerializeField] private Rigidbody arm1Rb;
+    [SerializeField] private Rigidbody arm2Rb;
+    [SerializeField] private Rigidbody arm3Rb;
+    [SerializeField] private Rigidbody handRotorRb;
+
+    [SerializeField] private float period = 5;
+    [SerializeField] private TMP_InputField inputInstructions;
+    [SerializeField] private string instructionsString;
+
+
+
+    public void RunCommand()
+    {
+        if (inputInstructions.text != null)
+        {
+            instructionsString = inputInstructions.text;
+            //hay que comprobar el formato correcto del comando
+            //los comandos deben tener el formato"M1_45,M2_-30"
+            StartCoroutine(InterpretInstructions(instructionsString));
+        }
+    }
+
+    private IEnumerator InterpretInstructions(string instruction)
+    {
+        string[] movimientos = instruction.Split(',');
+
+        foreach (string movimiento in movimientos)
+        {
+            string[] partes = movimiento.Split('_');
+
+            if (partes.Length == 2)
+            {
+                string eje = partes[0];
+                float grados = float.Parse(partes[1]);
+
+                yield return StartCoroutine(RealizarMovimiento(eje, grados));
+
+            }
+            else
+            {
+                Debug.LogError("Formato de instrucción incorrecto: " + movimiento);
+            }
+        }
+    }
+    private IEnumerator RealizarMovimiento(string eje, float grados)
+    {
+        float rotationTime = (period / 360) * grados;
+        switch (eje)
+        {
+            case "M1":
+                Debug.Log("Rotate Base");
+                baseRotorRb.DORotate(new Vector3(0, grados, 0), rotationTime, RotateMode.LocalAxisAdd);
+                break;
+
+            case "M2":
+                arm1Rb.DORotate(new Vector3(0, 0, grados), rotationTime, RotateMode.LocalAxisAdd);
+                break;
+
+            case "M3":
+                arm2Rb.DORotate(new Vector3(0, 0, grados), rotationTime, RotateMode.LocalAxisAdd);
+                break;
+
+            case "M4":
+                arm3Rb.DORotate(new Vector3(0, 0, grados), rotationTime, RotateMode.LocalAxisAdd);
+                break;
+
+            case "M5":
+                handRotorRb.DORotate(new Vector3(0, 0, grados), rotationTime, RotateMode.LocalAxisAdd);
+                break;
+        }
+
+       yield return null;
+    }
+}
